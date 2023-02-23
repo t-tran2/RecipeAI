69f27268ceb2f16ad947ac72c79d75304894a234
jittaneybrin <20jin.brittaney@gmail.com>
2023-02-23 17:23:11 -0500

    changes for checkpoint 3

--- recipeai/app/src/main/java/com/example/recipeai/ui/ingredients/IngredientsFragment.kt
+++ recipeai/app/src/main/java/com/example/recipeai/ui/ingredients/IngredientsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.recipeai.ui.ingredients
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,10 +19,12 @@ class IngredientsFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
+
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d("onCreateViewFragment", "onCreateView Fragment lifecycle triggered")
         val ingredientsViewModel =
             ViewModelProvider(this).get(IngredientsViewModel::class.java)
 
@@ -33,6 +36,20 @@ class IngredientsFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        Log.d("onDestroyViewFragment", "onDestroyView Fragment lifecycle triggered")
         _binding = null
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("onStartFragment", "onStart Fragment lifecycle triggered")
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("onDestroyFragment", "onDestroy Fragment lifecycle triggered")
+        _binding = null
+    }
+
 }
