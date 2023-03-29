be3cbd24d55d85a672fb787417fccb7278fb5f13
jittaneybrin <20jin.brittaney@gmail.com>
2023-03-28 21:01:21 -0400

    stuff works bare bones

--- recipeai/app/src/main/java/com/example/recipeai/ui/cooking/CookingViewModel.java
+++ recipeai/app/src/main/java/com/example/recipeai/ui/cooking/CookingViewModel.java
@@ -4,16 +4,41 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CookingViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private List<String> steps;
+    private int currentStepIndex;
 
     public CookingViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        steps = new ArrayList<>();
+        currentStepIndex= 0;
+        steps.add("hi");
+        steps.add("bye");
+        steps.add("hello");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void addStep(String step){
+        steps.add(step);
     }
+
+    public void nextStep(){
+        if(currentStepIndex<steps.size()-1) {
+            currentStepIndex++;
+        }
+    }
+
+    public void previousStep(){
+        if(currentStepIndex>0) {
+            currentStepIndex--;
+        }
+    }
+
+    public String getCurrentStep(){
+        return steps.get(currentStepIndex);
+    }
+
+
 }
